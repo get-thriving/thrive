6a15c9d81c44106fa882824fc15688172a10c55c
Horia Coman <horia141@gmail.com>
2025-09-20 13:05:29 +0300

    Remove unused scripts

--- scripts/check/check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-./scripts/check/lint.sh
-# ./scripts/check/audits.sh
-./scripts/check/check-all-is-formatted.sh
-./scripts/check/run-tests.sh
-./scripts/check/run-itests.sh ci
