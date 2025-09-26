0e79d397c4953aa239daa2a1dc39f3360879a325
Horia Coman <horia141@gmail.com>
2025-09-26 22:08:29 +0300

    A lot of stuff

--- scripts/setup-for-dev.sh
+++ scripts/setup-for-dev.sh
@@ -16,23 +16,6 @@ mkdir -p .build-cache
 python3 -m venv .build-cache/venv
 source .build-cache/venv/bin/activate
 
-brew bundle
-
-docker pull hadolint/hadolint:latest-debian
-
-bundle install
-
-poetry install --no-interaction --no-ansi
-(cd src/core && poetry install --no-interaction --no-ansi)
-(cd src/cli && poetry install --no-interaction --no-ansi)
-(cd src/webapi && poetry install --no-interaction --no-ansi)
-(cd src/docs && poetry install --no-interaction --no-ansi)
-(cd itests && poetry install --no-interaction --no-ansi)
-
-npm install --no-save
-(cd src/desktop && npm install --no-save)
-(cd src/mobile && npm install --no-save)
-
-playwright install
-
 (cd gen/ts/webapi-client && npx tsc)
+
+mise run --jobs=1 install
---
+++ tasks/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+#MISE description="Install dependencies"
+#MISE depends=["root:install", "package:**:install"]
