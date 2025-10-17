2b0467aaddbc1276bfa23ffb0d9eb01bcc9b95b2
Horia Coman <horia141@gmail.com>
2025-10-17 23:56:20 +0300

    Add the web task

---
+++ tasks/run/web.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+#MISE description="Run web app with optional namespace"
+#USAGE flag "--namespace <namespace>" help="Jupiter namespace"
+#USAGE complete "namespace" run="./tasks/run/namespace/_list-fast.sh"
+#USAGE flag "--log <log>" default="info" help="Log output" {
+#USAGE   choices "info" "debug" "trace"
+#USAGE }
+
+: "${usage_namespace:=}"
+
+set -e -o pipefail
+
+source tasks/_common.sh
+
+namespace="${usage_namespace}"
+
+if [[ -z "$namespace" ]]; then
+    namespace=$STANDARD_NAMESPACE
+fi
+
+webui_port=$(get_jupiter_port "$namespace" "webui")
+export HOSTED_GLOBAL_WEBUI_URL="http://localhost:$webui_port"
+
+log info "Running web in the browser app with namespace $namespace"
+
+open $HOSTED_GLOBAL_WEBUI_URL
