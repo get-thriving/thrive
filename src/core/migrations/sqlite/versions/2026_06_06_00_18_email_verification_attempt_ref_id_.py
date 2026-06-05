ca717f39f863c764ab36a89616d737c2947b9a99
Mike Bestcat <mike@get-thriving.com>
2026-06-06 00:33:03 +0300

    Merged branch bugfix/email-verification into develop

---
+++ src/core/migrations/sqlite/versions/2026_06_06_00_18_email_verification_attempt_ref_id_.py
@@ -0,0 +1,20 @@
+"""email verification attempt ref id identity
+
+Revision ID: c3cc27543077
+Revises: 9b53b5d8b981
+Create Date: 2026-06-06 00:18:00.984071
+
+"""
+
+revision = "c3cc27543077"
+down_revision = "9b53b5d8b981"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
